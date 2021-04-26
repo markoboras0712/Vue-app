0f9697476be1a4fefefb81724c57666a3fd34fa3
markoboras0712 <mboras1@etfos.hr>
2021-04-26 14:36:24 +0300

    nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '../views/EventList.vue'
-import EventDetails from '../views/EventDetails.vue'
-import About from '../views/About.vue'
-
+import About from '@/views/About.vue'
+import EventList from '@/views/EventList.vue'
+import EventDetails from '@/views/event/Details.vue'
+import EventRegister from '@/views/event/Register.vue'
+import EventEdit from '@/views/event/Edit.vue'
+import EventLayout from '../views/event/Layout.vue'
 const routes = [
   {
     path: '/',
@@ -12,9 +14,26 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      }
+    ]
   },
   {
     path: '/about',
